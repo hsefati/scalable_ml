0c85dcb8bfb08f4b5451d957fca84c6291758f6e
Hamed Sefati <hsefati@gmail.com>
2024-12-03 18:18:22 +0100

    tests: add more test for api call

--- tests/test_api.py
+++ tests/test_api.py
@@ -12,7 +12,35 @@ def test_greetings():
     assert response.json() == "Welcome to our model API"
 
 
-def test_inference_correct():
+def test_inference_correct_above_50k():
+    sample = {
+        "age": 52,
+        "workclass": "Self-emp-inc",
+        "fnlgt": 287927,
+        "education": "HS-grad",
+        "education_num": 9,
+        "marital_status": "Married-civ-spouse",
+        "occupation": "Exec-managerial",
+        "relationship": "Wife",
+        "race": "White",
+        "sex": "Female",
+        "capital_gain": 15024,
+        "capital_loss": 0,
+        "hours_per_week": 40,
+        "native_country": "United-States",
+    }
+    data = json.dumps(sample)
+
+    response = client.post("/inference/", data=data)
+
+    # test response and output
+    assert response.status_code == 200
+    assert response.json()["age"] == 52
+    assert response.json()["fnlgt"] == 287927
+    assert response.json()["prediction"] == ">50K"
+
+
+def test_inference_correct_below_50k():
     sample = {
         "age": 35,
         "workclass": "Self-emp-not-inc",
